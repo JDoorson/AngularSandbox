2bee177e17aa0b4b76cbcf585cc5c3a0915f382a
Jamal <ejdoorson@gmail.com>
2021-11-03 10:12:25 +0100

    Getting started - created match and player modules, including appropriate components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatchModule } from './match/match.module';
+import { PlayerModule } from './player/player.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatchModule,
+    PlayerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
